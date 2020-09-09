a5c26894e091b7cc91a23ca091ad7da0f559bfc4
omieli <mieli.ohad@gmail.com>
2020-09-09 07:52:41 +0300

    Ex2

--- Promises/dist/main.js
+++ Promises/dist/main.js
@@ -1,6 +1,10 @@
-$.get(`/randomWord`).then(function (word) {
-    return $.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${word}`)
-}).then(function (found) {
-  console.log(found)  
-})
-
+$.get('/randomWord')
+	    .then(function(word) {
+	        let book = $.get(`https://www.googleapis.com/books/v1/volumes?q=title:${word}`)
+	        let gif = $.get(`http://api.giphy.com/v1/gifs/search?q=${word}&api_key=HzsSoGIhud1VLofBt7XW6lzhrEKNOy6P`)
+	        Promise.all([book, gif]).then(function(found) {
+	                $("body").append(`<div>${found[0].items[0].volumeInfo.title}</div>`)
+	                $("body").append(`<iframe src="${found[1].data[0].embed_url}">`)
+	            })
+        })
+        
